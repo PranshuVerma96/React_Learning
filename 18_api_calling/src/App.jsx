4e8982ae1bd7618e0b762be2261774b6f9ad4dc7
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-04-25 07:21:33 +0530

    Add files via upload

---
+++ 18_api_calling/src/App.jsx
@@ -0,0 +1,42 @@
+import axios from 'axios'
+import React from 'react'
+import { useState } from 'react'
+const App = () => {
+
+  // fetch data using fetch method 
+    // const getData = async () => {
+    //   const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    //   const data = await response.json(response)
+    //   console.log(data);
+      
+    // }
+
+// fetch data from axios 
+    // const  getData = async () =>{
+    // const {data} =  await axios.get('https://jsonplaceholder.typicode.com/users')
+
+    // console.log(data);
+    
+    // }
+
+    const [data, setData] = useState([])
+    const getData =async () => {
+      const response =await axios.get('https://picsum.photos/v2/list')
+      
+      setData(response.data)
+      
+    }
+  return (
+  
+    <div>
+      <button onClick={getData}>Get Data</button>
+      <div>
+        {data.map(function (elem , idx){
+            return <h2>helo , {elem.author} {idx}</h2>
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
