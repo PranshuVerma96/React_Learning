23b4e65ac7e39e0908ebbc1aaa056119942eecc5
Pranshu Verma <er.pranshuverma68@gmail.com>
2026-04-25 23:26:15 +0530

    Add files via upload

---
+++ 21_react_Router_Dom/src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="nav">
+    <h3>Pranshu</h3>
+    <div>
+      {/* <a href="/">Home</a>
+      <a href="/about">About</a>
+      <a href="/contact">Contact</a> */}
+
+      <Link to='/' >Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/contact'>Contact</Link>
+      <Link to ='/product'>Product</Link>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ 21_react_Router_Dom/src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>Hello this is a about page </h1>
+    </div>
+  )
+}
+
+export default About
